3ff0e76856e6311e8d0e8645aa27a808d4009212
ALVIN MARCO CRISTY TAMPUBOLON <alvinmarco8@gmail.com>
2024-03-26 18:54:32 +0700

    update

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -1,5 +1,9 @@
 import Link from "next/link";
-const Nav = () => {
+import { getServerSession } from "next-auth";
+import { options } from "../api/auth/[...nextauth]/options";
+
+const Nav = async () => {
+  const session = await getServerSession(options);
   return (
     <header className="bg-gray-600 text-gray-100">
       <nav className="flex justify-between items-center w-full px-10 py-4">
@@ -10,6 +14,11 @@ const Nav = () => {
           <Link href="/ClientMember">Client Member</Link>
           <Link href="/Member">Member</Link>
           <Link href="/Public">Public</Link>
+          {session ? (
+            <Link href="/api/auth/signout?callbackUrl=/">Logout</Link>
+          ) : (
+            <Link href="/api/auth/signin">Login</Link>
+          )}
         </div>
       </nav>
     </header>
--- app/ClientMember/page.jsx
+++ app/ClientMember/page.jsx
@@ -1,6 +1,15 @@
 "use client";
 
-const Member = () => {
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+
+const Member = async () => {
+  const { data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
+      redirect("/api/auth/signin?callbackUrl=/ClientMember");
+    },
+  });
   return <div>Member</div>;
 };
 
